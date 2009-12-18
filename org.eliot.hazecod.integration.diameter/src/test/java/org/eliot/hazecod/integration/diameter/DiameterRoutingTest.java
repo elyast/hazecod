3e27835362c67a45563bdb7208772e1de9518800
lukasz.jastrzebski <lukasz.jastrzebski@localhost>
2009-12-18 16:24:36 +0100

    integration with Jdiameter finally works

--- org.eliot.hazecod.integration.diameter/src/test/java/org/eliot/hazecod/integration/diameter/DiameterRoutingTest.java
+++ org.eliot.hazecod.integration.diameter/src/test/java/org/eliot/hazecod/integration/diameter/DiameterRoutingTest.java
@@ -8,7 +8,10 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.mock.MockEndpoint;
+import org.jdiameter.api.Avp;
 import org.jdiameter.api.Message;
+import org.jdiameter.api.Request;
+import org.jdiameter.api.ResultCode;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
@@ -30,7 +33,7 @@ public class DiameterRoutingTest extends AbstractJUnit38SpringContextTests {
     @EndpointInject(uri = "mock:result")
     protected MockEndpoint resultEndpoint;
 
-    @Produce(ref = "myMinaEndpoint")
+    @Produce(ref = "diameterEndpoint")
     protected ProducerTemplate template;
 
     @Test
@@ -38,7 +41,7 @@ public class DiameterRoutingTest extends AbstractJUnit38SpringContextTests {
     public void testRouting() throws Exception {
 	Message expected = MessageFactory.createCEA();
 
-	Message sent = MessageFactory.createCER();
+	Request sent = MessageFactory.createCER();
 
 	// resultEndpoint.expectedBodiesReceived(expected);
 	template.sendBody(sent);
@@ -53,7 +56,8 @@ public class DiameterRoutingTest extends AbstractJUnit38SpringContextTests {
 	assertEquals(1, receivedMessagesCount);
 	assertEquals(expected.getCommandCode(), msg.getCommandCode());
 	assertEquals(expected.getApplicationId(), msg.getApplicationId());
-	assertEquals(FOUR, msg.getAvps().size());
+	Avp resultCode = msg.getAvps().getAvp(Avp.RESULT_CODE);
+	assertEquals(ResultCode.SUCCESS, resultCode.getInteger32());
 	resultEndpoint.assertIsSatisfied();
     }
 }
