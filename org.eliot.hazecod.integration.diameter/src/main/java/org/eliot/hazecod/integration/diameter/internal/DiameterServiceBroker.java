7f59fe4c436974ad46d763b98dbc28d2f5bc6355
lukasz.jastrzebski <lukasz.jastrzebski@localhost>
2010-01-07 10:08:31 +0100

    For acceptance test to pass

--- org.eliot.hazecod.integration.diameter/src/main/java/org/eliot/hazecod/integration/diameter/internal/DiameterServiceBroker.java
+++ org.eliot.hazecod.integration.diameter/src/main/java/org/eliot/hazecod/integration/diameter/internal/DiameterServiceBroker.java
@@ -1,6 +1,8 @@
 package org.eliot.hazecod.integration.diameter.internal;
 
 import org.eliot.hazecod.integration.diameter.ServiceBroker;
+import org.jdiameter.api.Answer;
+import org.jdiameter.api.AvpSet;
 import org.jdiameter.api.Request;
 import org.jdiameter.api.ResultCode;
 import org.slf4j.Logger;
@@ -8,20 +10,30 @@ import org.slf4j.LoggerFactory;
 
 /**
  * @author Eliot
- *
+ * 
  */
 public class DiameterServiceBroker implements ServiceBroker {
 
     static Logger logger = LoggerFactory.getLogger(DiameterServiceBroker.class);
+    static final int EVENT_REQUEST = 4;
+    static final int CC_REQUEST_NO = 415;
+    static final int CC_REQUEST_TYPE = 416;
+
     /**
-     * @param parameter Diameter Request
+     * @param parameter
+     *            Diameter Request
      * @return Diameter Answer
      */
     @Override
     public Object handle(Object parameter) {
 	Request request = (Request) parameter;
 	logger.error("DiameterServiceBroker - fatal error");
-	return request.createAnswer(ResultCode.SUCCESS);
+	logger.info("DiameterServiceBroker - fatal error");
+	Answer answer = request.createAnswer(ResultCode.SUCCESS);
+	AvpSet avpSet = answer.getAvps();
+	avpSet.addAvp(CC_REQUEST_TYPE, EVENT_REQUEST);
+	avpSet.addAvp(CC_REQUEST_NO, 0L, 0L, true, false, true);
+	return answer;
     }
 
 }
