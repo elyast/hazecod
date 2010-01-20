5b5e863cd09373e13dceaa672b5767a054e9daa0
wro02896 <wro02896@localhost>
2010-01-20 13:54:52 +0100

    Javadoc added

--- org.eliot.hazecod.billing/src/main/java/org/eliot/hazecod/billing/internal/BillingResultImpl.java
+++ org.eliot.hazecod.billing/src/main/java/org/eliot/hazecod/billing/internal/BillingResultImpl.java
@@ -2,8 +2,15 @@ package org.eliot.hazecod.billing.internal;
 
 import org.eliot.hazecod.billing.BillingResult;
 
+/**
+ * @author Eliot
+ *
+ */
 public class BillingResultImpl implements BillingResult {
 
+    /**
+     * @return <code>true</code> if succeded
+     */
     @Override
     public boolean isSucceded() {
 	// TODO Auto-generated method stub
--- org.eliot.hazecod.billing/src/main/java/org/eliot/hazecod/billing/internal/UnitImpl.java
+++ org.eliot.hazecod.billing/src/main/java/org/eliot/hazecod/billing/internal/UnitImpl.java
@@ -2,14 +2,25 @@ package org.eliot.hazecod.billing.internal;
 
 import org.eliot.hazecod.billing.Unit;
 
+/**
+ * @author Eliot
+ *
+ * @param <V> Value
+ */
 public class UnitImpl<V> implements Unit<V> {
 
+    /**
+     * @return unit type
+     */
     @Override
     public org.eliot.hazecod.billing.Unit.UnitType getType() {
 	// TODO Auto-generated method stub
 	return null;
     }
 
+    /**
+     * @return value
+     */
     @Override
     public V getValue() {
 	// TODO Auto-generated method stub
